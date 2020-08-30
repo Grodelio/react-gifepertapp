31ba8f5ded20e90e3d8c58851673a908253b25b2
Antonio <antonioblancoruiz@gmail.com>
2020-08-30 22:16:49 +0200

    docs agregado

---
+++ docs/precache-manifest.2a3ec36f7745bd05ebfbdea08f701fc0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3dfd2698c32fc22887ca5347ea4a298",
+    "url": "/index.html"
+  },
+  {
+    "revision": "248423f6dd6ead7ffd10",
+    "url": "/static/css/2.e2dd9b0a.chunk.css"
+  },
+  {
+    "revision": "1914d7bc36b1c3fc2c78",
+    "url": "/static/css/main.e2f77ac3.chunk.css"
+  },
+  {
+    "revision": "248423f6dd6ead7ffd10",
+    "url": "/static/js/2.1a68ba2f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1a68ba2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1914d7bc36b1c3fc2c78",
+    "url": "/static/js/main.483a72c1.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
